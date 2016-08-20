89c05e5ab04a9360546fcf15f3593de72b4a84fd
Katherine G. Pe <katherinepe@me.com>
2016-08-20 22:42:53 +0800

    Test multiple random users

--- test/random_user/random_test.exs
+++ test/random_user/random_test.exs
@@ -3,12 +3,19 @@ defmodule RandomUser.RandomTest do
   use ExVCR.Mock
 
   describe "one" do
-    test "get one random user with ibrowse" do
+    test "get one random user" do
       use_cassette "one_random_user" do
         assert RandomUser.Random.one.body["info"]["results"] == 1
       end
     end
+  end
 
+  describe "multiple" do
+    test "get multiple random user" do
+      use_cassette "multiple_random_users" do
+        assert RandomUser.Random.multiple(10).body["info"]["results"] == 10
+      end
+    end
   end
 
   doctest RandomUser.Random
