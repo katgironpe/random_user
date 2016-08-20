7bc73dabe7b5ecc18e7734dc2c10b811768b2822
Katherine G. Pe <katherinepe@me.com>
2016-08-20 22:04:09 +0800

    Test one random user

--- test/random_user/random_test.exs
+++ test/random_user/random_test.exs
@@ -1,5 +1,15 @@
 defmodule RandomUser.RandomTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
+  use ExVCR.Mock
+
+  describe "one" do
+    test "get one random user with ibrowse" do
+      use_cassette "one_random_user" do
+        assert RandomUser.Random.one.body["info"]["results"] == 1
+      end
+    end
+
+  end
 
   doctest RandomUser.Random
 end
