df41a3da1f2320eeea14fe2944f46390197fd26d
Katherine G. Pe <katherinepe@me.com>
2016-08-21 13:35:20 +0800

    Test parser results for multiple users

--- test/random_user/parser_test.exs
+++ test/random_user/parser_test.exs
@@ -11,9 +11,17 @@ defmodule RandomUser.ParserTest do
   end
 
   describe "RandomUser.Parser.results/1" do
-    test "retrieves results" do
+    test "retrieves results for one user" do
       use_cassette "one_random_user" do
         user = RandomUser.Random.one |> RandomUser.Parser.results
+        assert user["picture"]["large"] =~ ~r/https:\/\/randomuser.me\/api\/portraits/
+        assert user["picture"]["medium"] =~ ~r/https:\/\/randomuser.me\/api\/portraits\/med/
+      end
+    end
+
+    test "retrieves results for multiple users" do
+      use_cassette "multiple_random_users" do
+        user = RandomUser.Random.multiple(10) |> RandomUser.Parser.results |> List.first
         assert user["picture"]["large"] =~ ~r/https:\/\/randomuser.me\/api\/portraits/
         assert user["picture"]["medium"] =~ ~r/https:\/\/randomuser.me\/api\/portraits\/med/
       end
